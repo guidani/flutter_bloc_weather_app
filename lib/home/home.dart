3dfbd30952e134868eb06cb3271e07ff226986cf
guilherme daniel <gui.dani13@gmail.com>
2022-07-04 16:24:10 -0300

    concluido

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:bored_api/data/models/BoredActivityModel.dart';
 import 'package:bored_api/home/bloc/home_bloc.dart';
 import 'package:bored_api/services/boredService.dart';
 import 'package:flutter/material.dart';
@@ -19,28 +20,35 @@ class HomePage extends StatelessWidget {
         child: BlocBuilder<HomeBloc, HomeState>(
           builder: (context, state) {
             if (state is HomeLoadingState) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
+              return _buildLoadingState();
             }
             if (state is HomeLoadedState) {
-              return Column(
-                children: [
-                  Text(state.boredActivityModel.activity),
-                  Text(state.boredActivityModel.type),
-                  Text(state.boredActivityModel.participants.toString()),
-                  ElevatedButton(
-                    onPressed: () =>
-                        BlocProvider.of<HomeBloc>(context).add(LoadApiEvent()),
-                    child: Text('LOAD EVENT'),
-                  ),
-                ],
-              );
+              return _buildLoadedWidgetState(context, state.boredActivityModel);
             }
             return Container();
           },
         ),
       ),
+    );
+  }
+
+  Widget _buildLoadingState() {
+    return const Center(child: CircularProgressIndicator());
+  }
+
+  Widget _buildLoadedWidgetState(
+      BuildContext context, BoredActivityModel boredActivityModel) {
+    return Column(
+      children: [
+        Text(boredActivityModel.activity),
+        Text(boredActivityModel.type),
+        Text(boredActivityModel.participants.toString()),
+        ElevatedButton(
+          onPressed: () =>
+              BlocProvider.of<HomeBloc>(context).add(LoadApiEvent()),
+          child: Text('LOAD EVENT'),
+        ),
+      ],
     );
   }
 }
